a1700b567f2894dbf99e70d2f8492679075de0d6
Luiz A. M. Ferreira <luiz_fantasma@hotmail.com>
2021-05-08 02:38:50 -0300

    Defined a constant for the 2sc challenge lisp code.

--- Arcade/Intro/2.lisp
+++ Arcade/Intro/2.lisp
@@ -1,5 +1,6 @@
 (defconstant CENTURY 100
   "Define a constant for the value of a century.")
+
 (defun centuryFromYear (year)
   "Given a year its return the century."
   (ceiling (/ year CENTURY)))

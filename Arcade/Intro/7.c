c2e00759f7546bd5c41ae166c976d448aa52f457
Luiz A. M. Ferreira <luiz_fantasma@hotmail.com>
2021-05-10 19:47:52 -0300

    Refactor 7.c

--- Arcade/Intro/7.c
+++ Arcade/Intro/7.c
@@ -31,27 +31,13 @@ bool is_sorted_from(arr_integer s, int i) {
   return true;
 }
 
-bool is_sorted_without_pos(arr_integer s, int pos) {
-  int i;
-  for(i = 0; (i + 1) < s.size; i++) {
-    if(i == pos) continue;       /* Case i is pos, ignore  */
-    if(i + 1 == pos && i + 2 == s.size) return true; /* case pos is last */
-    if(i + 1 == pos) {                             /* Case next is pos */
-      if(s.arr[i] >= s.arr[i + 2]) return false;
-    }
-    else {
-      if(s.arr[i] >= s.arr[i + 1]) return false;
-    }
-  }
-  return true;
-}
-
 bool almostIncreasingSequence(arr_integer sequence) {
   /*Determine whether it is possible to obtain a strictly increasing
     sequence by removing no more than one element from the array.*/
   int idx = find_position_of_not_increasing(sequence);
-  if(idx + 1 == sequence.size) return true; /* idx as last position */
-  if(idx == 0) return is_sorted_from(sequence, idx + 1); /* idx is first */
-  if(is_sorted_without_pos(sequence, idx)) return true;
-  return is_sorted_without_pos(sequence, idx + 1);
+  if(idx == 0) return is_sorted_from(sequence, idx + 1); /* pos is 1st element */
+  if((idx + 1) >= sequence.size || (idx + 2) >= sequence.size) return true; /* Or does not exist pos or pos is last */
+  if(sequence.arr[idx - 1] < sequence.arr[idx + 1]) return is_sorted_from(sequence, idx + 1);
+  if(sequence.arr[idx] < sequence.arr[idx + 2]) return is_sorted_from(sequence, idx + 2);
+  return false;
 }

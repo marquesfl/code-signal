8ef142800f7105ca10bded628f28bdd77b7c3509
Luiz A. M. Ferreira <luiz_fantasma@hotmail.com>
2021-05-10 21:35:23 -0300

    Made the 8th challange. It was made a sum of transpose. Remember that in code signal the access for matrix is different to lisp.

---
+++ Arcade/Intro/8.cpp
@@ -0,0 +1,12 @@
+int matrixElementsSum(std::vector<std::vector<int>> matrix) {
+  // Sum element of transpose matrix while not zero element.
+  int i, j, s;
+  s = 0;                        // To summation
+  for(j = 0; j < matrix[0].size(); j++)
+    for(i = 0; i < matrix.size(); i++) {
+      if(matrix[i][j] == 0) break;
+      s += matrix[i][j];
+    }
+
+  return s;
+}

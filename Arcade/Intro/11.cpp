71821f498097522b655395fe000f8620d9f74b27
Luiz A. M. Ferreira <luiz_fantasma@hotmail.com>
2021-05-11 23:37:10 -0300

    Refactor 11.cpp

--- Arcade/Intro/11.cpp
+++ Arcade/Intro/11.cpp
@@ -1,17 +1,18 @@
 #include<cmath>
 
+int summation10(int& n, int len) {
+  int sum = 0;
+  for(int i = 0; i < len; n /= 10, i++)
+    sum += n % 10;
+  return sum;
+}
 bool isLucky(int n) {
   // Determine if n is a luck number. To be luck, he sum of the first
   // half of the digits is equal to the sum of the second half.
   int half_dig = ((int) log10(n) + 1) >> 1; // Determine the quantity
                                            // of half of digits of n
-  int sum1, sum2;
-  sum1 = sum2 = 0;              // Summation
-  for(int i = 0; i < half_dig; n /= 10, i++) // Sum 1st half
-    sum1 += n % 10;
-  for(int i = 0; i < half_dig; n /= 10, i++)
-    sum2 += n % 10;
-  return sum1 == sum2;
+  return summation10(n, half_dig)
+    == summation10(n, half_dig);
 }
     
 

a8133bac1eb4c582404cd0e26f3a622106f2811f
Luiz A. M. Ferreira <luiz_fantasma@hotmail.com>
2021-05-14 18:42:45 -0300

    I thought must more elegant.

--- Arcade/Intro/14.clj
+++ Arcade/Intro/14.clj
@@ -1,10 +1,5 @@
-(defn sum_ [f l]
-  "Sum of alternative elements."
-  (cond
-    (empty? l) 0
-    (not f) (sum_ (not f) (rest l))
-    :otherwise (+ (first l) (sum_ (not f) (rest l)))))
-
 (defn alternatingSums [a]
-  "Given a list do a sum of alternative elements."
-  [(sum_ true a) (sum_ false a)])
+  "Alternating sum.
+   By froks."
+  [(apply + (take-nth 2 a))             ; First sum
+   (apply + (take-nth 2 (rest a)))])    ; Second sum

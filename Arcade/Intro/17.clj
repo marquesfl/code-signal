d6b1f60580551bd1a3576de085c1d6d791f9262d
Luiz A. M. Ferreira <luiz_fantasma@hotmail.com>
2021-05-18 00:33:14 -0300

    Beatifull, I did not know about recur, it is pratically a lambda function with recursion with loop. It is just perfect.

---
+++ Arcade/Intro/17.clj
@@ -0,0 +1,12 @@
+(defn arrayChange [inputArray]
+  "arrayChange : [Int] -> Int.
+   Increase by one the elements inside inputArray until its
+   increasing array."
+  (loop [[first_ second_ & rest_] inputArray
+         moves 0]
+    (cond
+      (nil? second_) moves
+      (< first_ second_) (recur (cons second_ rest_) moves)
+      :otherwise (let [diff (inc (- first_ second_))]
+                   (recur (cons  (inc first_) rest_) (+ moves diff))))))
+
